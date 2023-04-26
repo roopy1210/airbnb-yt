84db2e407db98c1bdbbdf831e68287b11e6de0eb
roopy <swjeong1210@gmail.com>
2023-04-26 21:11:30 +0900

    update source

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script
-          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=322d26b42028e722d36053c5684e1770&libraries=services,clusterer&autoload=false"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=&libraries=services,clusterer&autoload=false"
           strategy="beforeInteractive"
         />
       </body>
